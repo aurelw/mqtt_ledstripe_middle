69f7037fb304f19451b480a65abf9453bc89a533
Aurel Wildfellner <aurel.w@gmail.com>
2016-03-26 12:58:05 +0100

    Async Animation

--- mqtt_ledstripe_middle.py
+++ mqtt_ledstripe_middle.py
@@ -24,30 +24,70 @@ import os
 
 import mosquitto
 import topicconfig
+import time
 from topictypes import TopicTypes
 
 
 class LedStripe:
 
 
+
     def __init__(self, client):
         #self.stripedata = b"\xff"*150
-        self.stripestring = "r"*50
+        self.stripelength = 50
+        self.stripestring = "r"*self.stripelength
         self.client = client
         self.topic = "devlol/things/18:FE:34:9C:5D:09/set"
 
+        self._callstack = []
+
     def pushPixel(self, pixel):
-        self.stripedata = (pixel + self.stripedata)[:150]
+        self.stripedata = (pixel + self.stripedata)[:self.stripelength*3]
         print("stripedata:", self.stripedata)
         #self.client.publish(self.topic, bytearray(self.stripedata))
 
-    def pushPalette(self, color):
-        self.stripestring = (color + self.stripestring)[:50]
+    def _pubStripeString(self):
+        print("pub")
         self.client.publish(self.topic, self.stripestring)
 
+    def pushPalette(self, color):
+        self.stripestring = (color + self.stripestring)[:self.stripelength]
+        self._pubStripeString()
+
     def setStripe(self, color):
-        self.stripedata = color*50
-        self.client.publish(self.topic, self.stripedata)
+        self.stripedata = color*self.stripelength
+        self._pubStripeString()
+
+    def animateClear(self):
+        a = 1  #leds per second**2
+        for i in range(1,self.stripelength+1):
+            self.pushPalette("n")
+            time.sleep( 1.0 / (1.0 + a*i))
+
+
+    def animateBlink(self):
+        self._callstack.insert(len(self._callstack), self._animateBlink)
+
+    def _animateBlink(self):
+        sstring = self.stripestring
+        speed = 10.0
+        for i in range(100):
+            self.pushPalette("n")
+            time.sleep(1.0 / speed)
+            self.pushPalette("n")
+            time.sleep(1.0 / speed)
+            self.pushPalette("w")
+            time.sleep(1.0 / speed)
+
+        self.stripestring = sstring
+        self._pubStripeString()
+
+
+    def doAnimations(self):
+        for f in self._callstack:
+            f()
+        self._callstack = []
+
 
 
 
@@ -61,6 +101,14 @@ def on_message(client, ledstripe, msg):
             if ctopic['type'] == TopicTypes.PUSH_PIXEL_ON_PAYLOAD:
                 if "payload" in ctopic.keys() and payload == ctopic['payload']:
                     ledstripe.pushPalette(ctopic['pcolor'])
+
+            if ctopic['type'] == TopicTypes.ANIMATE_CLEAR_ON_TOPIC:
+                ledstripe.animateClear()
+                    
+            if ctopic['type'] == TopicTypes.ANIMATE_BLINK_ON_PAYLOAD:
+                if "payload" in ctopic.keys() and payload == ctopic['payload']:
+                    print("Animate Blink")
+                    ledstripe.animateBlink()
                     
 
 
@@ -109,6 +157,7 @@ def main():
     while True:
         
         client.loop()
+        ledstripe.doAnimations()
 
 
 if __name__ == "__main__":
--- topicconfig.py
+++ topicconfig.py
@@ -1,9 +1,12 @@
 from topictypes import TopicTypes
 
 ledstripe_topics = [
-        {"type": TopicTypes.PUSH_PIXEL_ON_PAYLOAD, "topic": "craftui/button/bred", "payload": "DOWN", "pixel": b"\xff\x00\x00", "pcolor": "r"},
-        {"type": TopicTypes.PUSH_PIXEL_ON_PAYLOAD, "topic": "craftui/button/bgreen", "payload": "DOWN", "pixel": b"\x00\xff\x00", "pcolor": "g"},
-        {"type": TopicTypes.PUSH_PIXEL_ON_PAYLOAD, "topic": "craftui/button/bblue", "payload": "DOWN", "pixel": b"\x00\x00\xff", "pcolor": "b"},
+        {"type": TopicTypes.PUSH_PIXEL_ON_PAYLOAD, "topic": "craftui/button/bwink", "payload": "DOWN", "pixel": b"\xff\x00\x00", "pcolor": "nwn"},
+        {"type": TopicTypes.PUSH_PIXEL_ON_PAYLOAD, "topic": "craftui/button/bred", "payload": "DOWN", "pixel": b"\xff\x00\x00", "pcolor": "rrr"},
+        {"type": TopicTypes.PUSH_PIXEL_ON_PAYLOAD, "topic": "craftui/button/bgreen", "payload": "DOWN", "pixel": b"\x00\xff\x00", "pcolor": "ggg"},
+        {"type": TopicTypes.PUSH_PIXEL_ON_PAYLOAD, "topic": "craftui/button/bblue", "payload": "DOWN", "pixel": b"\x00\x00\xff", "pcolor": "bbb"},
+        {"type": TopicTypes.PUSH_PIXEL_ON_PAYLOAD, "topic": "craftui/button/bblue", "payload": "DOWN", "pixel": b"\x00\x00\xff", "pcolor": "bbb"},
+        {"type": TopicTypes.ANIMATE_BLINK_ON_PAYLOAD, "topic": "craftui/button/bstripe", "payload": "DOWN"},
 
 ]
 
